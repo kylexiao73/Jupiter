c283a5f462b26bab110d0a24011fefc1840f6133
kylexiao73 <xiao.lin@husky.neu.edu>
2018-05-01 23:41:43 -0400

    First commit.

---
+++ src/db/mysql/MySQLConnection.java
@@ -0,0 +1,226 @@
+package db.mysql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import db.DBConnection;
+import entity.Item;
+import entity.Item.ItemBuilder;
+import external.TicketMasterAPI;
+
+
+public class MySQLConnection implements DBConnection {
+	private Connection conn;
+
+	public MySQLConnection() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver").getConstructor().newInstance();
+			conn = DriverManager.getConnection(MySQLDBUtil.URL);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	@Override
+	public void close() {
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+	}
+
+	@Override
+	public void setFavoriteItems(String userId, List<String> itemIds) {
+		// TODO Auto-generated method stub
+		if (conn == null) {
+			return;
+		}
+		String sql = "INSERT IGNORE INTO history (user_id,item_id) VALUES(?,?)";
+		try {
+			for(String itemId: itemIds) {
+				PreparedStatement statement = conn.prepareStatement(sql);
+				statement.setString(1, userId);
+				statement.setString(2, itemId);
+				statement.executeUpdate();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	@Override
+	public void unsetFavoriteItems(String userId, List<String> itemIds) {
+		// TODO Auto-generated method stub
+		if (conn == null) {
+			return;
+		}
+		String sql = "DELETE FROM history WHERE user_id = ? AND item_id = ?";
+		try {
+			for (String itemId : itemIds) {
+				PreparedStatement statement = conn.prepareStatement(sql);
+				statement.setString(1, userId);
+				statement.setString(2, itemId);
+				statement.executeUpdate();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public Set<String> getFavoriteItemIds(String userId) {
+		// TODO Auto-generated method stub
+		
+		Set<String> itemIds = new HashSet<>();
+		if (conn == null) {
+			return itemIds;
+		}
+		String sql = "SELECT item_id FROM history WHERE user_id = ?";
+		try {
+			PreparedStatement statement = conn.prepareStatement(sql);
+			statement.setString(1, userId);
+			
+			ResultSet rs = statement.executeQuery();
+			while (rs.next()) {
+				itemIds.add(rs.getString("item_id"));
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return itemIds;
+	}
+
+	@Override
+	public Set<Item> getFavoriteItems(String userId) {
+		// TODO Auto-generated method stub
+		Set<Item> items = new HashSet<>();
+		if (conn == null) {
+			return items;
+		}
+		//1st step:
+		Set<String> itemIds = getFavoriteItemIds(userId);
+		String sql = "SELECT * FROM items WHERE item_id = ?";
+
+		try {
+			for (String itemId : itemIds) {
+				PreparedStatement statement = conn.prepareStatement(sql);
+				statement.setString(1, itemId);
+				ResultSet rs = statement.executeQuery();
+				// [ {“name”: “abcd”, “rating”: 0, “address”:”abcd”, ...} ]
+
+				while (rs.next()) {
+					ItemBuilder builder = new ItemBuilder();
+					builder.setItemId(rs.getString("item_id"));
+					builder.setName(rs.getString("name"));
+					builder.setRating(rs.getDouble("rating"));
+					builder.setAddress(rs.getString("address"));
+					builder.setImageUrl(rs.getString("image_url"));
+					builder.setUrl(rs.getString("url"));
+					builder.setDistance(rs.getDouble("distance"));
+					builder.setCategories(getCategories(itemId));
+					
+					items.add(builder.build());
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return items;
+
+	}
+
+	@Override
+	public Set<String> getCategories(String itemId) {
+		// TODO Auto-generated method stub
+		Set<String> categories = new HashSet<>();
+		if (conn == null) {
+			return categories;
+		}
+		String sql = "SELECT category FROM categories WHERE item_id = ?";
+		try {
+			PreparedStatement statement = conn.prepareStatement(sql);
+			statement.setString(1, itemId);
+			
+			ResultSet rs = statement.executeQuery();
+			while (rs.next()) {
+				categories.add(rs.getString("category"));
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return categories;
+	}
+
+	@Override
+	public void saveItem(Item item) {
+		// TODO Auto-generated method stub
+		if(conn==null) return;
+		try {
+			//1st: insert into table: need to check if the same item has inserted already using keyword "IGNORE"
+			String sql = "INSERT IGNORE INTO items VALUES (?,?,?,?,?,?,?)";
+			PreparedStatement statement = conn.prepareStatement(sql);
+			statement.setString(1, item.getItemId());
+			statement.setString(2, item.getName());
+			statement.setDouble(3, item.getRating());
+			statement.setString(4, item.getAddress());
+			statement.setString(5, item.getImageUrl());
+			statement.setString(6, item.getUrl());
+			statement.setDouble(7, item.getDistance());
+			statement.executeUpdate();
+			
+			//2nd: update categories table for each categories
+			sql = "INSERT IGNORE INTO categories VALUES(?,?)";
+			for(String category: item.getCategories()) {
+				statement = conn.prepareStatement(sql);
+				statement.setString(1, item.getItemId());
+				statement.setString(2, category);
+				statement.execute();
+
+			}
+			
+			
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+	@Override
+	public List<Item> searchItems(double lat, double lon, String term) {
+		// TODO Auto-generated method stub
+		TicketMasterAPI tmAPI = new TicketMasterAPI();
+		List<Item> items = tmAPI.search(lat,lon,term);
+		for (Item item : items) {
+			saveItem(item);
+		}
+		return items;
+	}
+
+
+	@Override
+	public String getFullname(String userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean verifyLogin(String userId, String password) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ src/rpc/recommendation.java
@@ -0,0 +1,69 @@
+package rpc;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import algorithm.GeoRecommendation;
+import entity.Item;
+
+/**
+ * Servlet implementation class recommendation
+ */
+@WebServlet("/recommendation")
+public class recommendation extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public recommendation() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String userId = request.getParameter("user_id");
+		Double lat = Double.parseDouble(request.getParameter("lat"));
+		Double lon = Double.parseDouble(request.getParameter("lon"));
+
+		GeoRecommendation recommendation = new GeoRecommendation();
+		List<Item> recommendedItems = recommendation.recommendItems(userId, lat, lon);
+
+		JSONArray array = new JSONArray();
+		try {
+			for (Item item : recommendedItems) {
+				array.put(item.toJSONObject());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		RPCHelper.writeJsonArray(response, array);	
+	}
+
+
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
